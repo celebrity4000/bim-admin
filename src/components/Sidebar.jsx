04655d895b0e37d20e27177e03c6d68d07bc46e9
Rohan Gope <rg2512@it.jgec.ac.in>
2024-10-04 15:19:43 +0530

    refactor: Remove unused imports and optimize sidebar rendering

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import {
   MdDashboard,
-  MdUpcoming,
   MdContactSupport,
   MdOutlineSettings,
   MdAssignmentAdd,
@@ -21,11 +20,7 @@ import {
 import { FiUsers } from "react-icons/fi";
 import { SiHelpdesk } from "react-icons/si";
 import { MdWifiCalling3 } from "react-icons/md";
-import {
-  FaBarsProgress,
-  FaMoneyCheckDollar,
-  TbStackFilled,
-} from "react-icons/fa6";
+import { FaBarsProgress, FaMoneyCheckDollar } from "react-icons/fa6";
 import { IoMdAnalytics } from "react-icons/io";
 
 const sidebarData = [
@@ -33,12 +28,19 @@ const sidebarData = [
     navigate: "/overview",
     name: "Overview",
     icon: <MdDashboard size={30} />,
-    isDropdown: false,
-  },
-  {
-    navigate: "/trainingMetrics",
-    name: "Training Metrics",
-    icon: <TbStackFilled size={30} />,
+    isDropdown: true,
+    items: [
+      {
+        name: "Upcoming Seasons",
+        navigate: "/upcomingSeasons",
+        icon: <MdDashboard size={25} />,
+      },
+      {
+        name: "Training Metrics",
+        navigate: "/trainingMetrics",
+        icon: <MdDashboard size={25} />,
+      },
+    ],
   },
   {
     navigate: "/blog",
@@ -201,23 +203,52 @@ function Sidebar() {
 
   return (
     <div className="w-[300px] bg-[#fff] min-h-[calc(100vh-80px)] sticky top-[80px]">
-      <div className="flex h-full max-h-screen flex-col gap-2 sticky top-0 ">
-        <div className=" overflow-auto pl-2 sticky top-[90px]">
-          <nav className="grid gap-[20px] items-start pl-4  text-sm font-medium">
-            {sidebarData.map((item) => (
-              <NavLink
-                key={item.name}
-                className={({ isActive }) =>
-                  ` flex items-center gap-3 rounded-l-[10px] px-3 py-2  transition-all hover:text-darkgray dark:text-gray-400 dark:hover:text-gray-50 ${
-                    isActive
-                      ? "bg-[#F5F5F5] border-r-4 border-pink text-darkgray font-[600]"
-                      : "text-[#455560] "
-                  }`
-                }
-                to={item.navigate}>
-                {item.icon}
-                {item.name}
-              </NavLink>
+      <div className="flex h-full max-h-screen flex-col gap-2 sticky top-0">
+        <div className="overflow-auto pl-2 sticky top-[90px]">
+          <nav className="grid gap-[20px] items-start pl-4 text-sm font-medium">
+            {sidebarData.map((item, index) => (
+              <div key={item.name}>
+                <NavLink
+                  className={({ isActive }) =>
+                    `flex justify-between items-center rounded-l-[10px] px-3 py-2 transition-all hover:text-darkgray dark:text-gray-400 dark:hover:text-gray-50 ${
+                      isActive && !item.isDropdown
+                        ? "bg-[#F5F5F5] border-r-4 border-pink text-darkgray font-[600]"
+                        : "text-[#455560]"
+                    }`
+                  }
+                  to={item.isDropdown ? "#" : item.navigate}
+                  onClick={() => item.isDropdown && toggleDropdown(index)}>
+                  <div className="flex items-center gap-3">
+                    {item.icon}
+                    {item.name}
+                  </div>
+                  <div className="justify-end">
+                    {item.isDropdown && <RiArrowDropDownFill size={20} />}
+                  </div>
+                  <></>
+                </NavLink>
+
+                {/* Dropdown Section */}
+                {item.isDropdown && dropdownOpen === index && (
+                  <div className="ml-6 mt-2 space-y-2">
+                    {item.items.map((subItem) => (
+                      <NavLink
+                        key={subItem.name}
+                        className={({ isActive }) =>
+                          `flex items-center gap-2 rounded-l-[10px] px-3 py-2 transition-all hover:text-darkgray dark:text-gray-400 dark:hover:text-gray-50 ${
+                            isActive
+                              ? "bg-[#F5F5F5] border-r-4 border-pink text-darkgray font-[600]"
+                              : "text-[#455560]"
+                          }`
+                        }
+                        to={subItem.navigate}>
+                        {subItem.icon}
+                        {subItem.name}
+                      </NavLink>
+                    ))}
+                  </div>
+                )}
+              </div>
             ))}
           </nav>
         </div>
